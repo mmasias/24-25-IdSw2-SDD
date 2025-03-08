ff88be36e576923dce00ae3e6793db3a1fdd87c1
jorgeog25 <jrotero258@gmail.com>
2025-03-08 12:47:40 +0100

    Clase ascensor

--- pyAscensores/Ascensor.java
+++ pyAscensores/Ascensor.java
@@ -1,5 +1,32 @@
 package pyAscensores;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ascensor {
-    
+    private int capacidadMaxima;
+    private int pisoActual;
+    private List<Persona> personas;
+    private int estado; // 0: detenido, 1: subiendo, -1: bajando
+
+    public Ascensor(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+        this.pisoActual = 0;
+        this.personas = new ArrayList<Persona>();
+        this.estado = 0;
+    }
+
+    public boolean puedeRecogerPersona() {
+        return personas.size() < capacidadMaxima;
+    }
+
+    public void recogerPersona(Persona persona) {
+        if(puedeRecogerPersona()) {
+            personas.add(persona);
+        }
+    }
+
+    public int getPlantaActual() {
+        return pisoActual;
+    }
 }
