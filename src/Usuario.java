061db4399eb07ab45685a0e01bfaef2634635d7d
Santiago Gómez de la Torre Romero <gomezdelatorre15@gmail.com>
2025-04-15 16:05:27 +0200

    FIX/ cambios solicitados

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,58 +1,56 @@
 package src;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class Usuario {
-    private double efectivo;
+    private Map<Double, Integer> efectivo;
     private double tarjetaMonedero;
     private double tarjetaBancaria;
 
     public Usuario() {
-        Random random = new Random();
-        this.efectivo = generarAleatorioConPasos(1, 50, 1);
-        this.tarjetaMonedero = generarAleatorioConPasos(1, 40, 1);
-        this.tarjetaBancaria = generarAleatorioConPasos(1, 40, 1);
+        this.efectivo = new HashMap<>();
+        inicializarEfectivo();
+        this.tarjetaMonedero = generarSaldoInicial(1, 40, 1);
+        this.tarjetaBancaria = generarSaldoInicial(1, 40, 1);
     }
 
-    private double generarAleatorioConPasos(int min, int max, int pasos) {
+    private void inicializarEfectivo() {
+        double[] denominaciones = {0.05, 0.10, 0.20, 0.50, 1.0, 2.0, 5.0, 10.0, 20.0};
+        for (double denom : denominaciones) {
+            efectivo.put(denom, (int) generarSaldoInicial(0, 10, 1));
+        }
+    }
+
+    private double generarSaldoInicial(int min, int max, int pasos) {
         Random random = new Random();
         int rango = ((max - min) / pasos) + 1;
         return min + (random.nextInt(rango) * pasos);
     }
 
-    public void seleccionarMaquina(String numMaquina) {
-        System.out.println("Seleccionando la máquina " + numMaquina);
+    public boolean seleccionarMaquina(String numMaquina) {
+        return numMaquina != null && !numMaquina.isEmpty();
     }
 
-    public void ingresarSeleccion(String producto) {
-        System.out.println("Producto seleccionado: " + producto);
+    public boolean ingresarSeleccion(String producto) {
+        return producto != null && !producto.isEmpty();
     }
     
-    public void elegirMetodoPago() {
-        System.out.println("Elegir método de pago:");
-        System.out.println("1. Efectivo");
-        System.out.println("2. Tarjeta Monedero");
-        System.out.println("3. Tarjeta Bancaria");
+    public String elegirMetodoPago() {
+        return "EFECTIVO";
     }
 
-    public void ingresarEfectivo() {
-        System.out.println("Ingresa el efectivo:");
-        System.out.println("1. Moneda de €0,05");
-        System.out.println("2. Moneda de €0,20");
-        System.out.println("3. Moneda de €0,50");
-        System.out.println("4. Moneda de €1");
-        System.out.println("5. Moneda de €2");
-        System.out.println("6. Billete de €5");
-        System.out.println("7. Billete de €10");
-        System.out.println("8. Billete de €20");
+    public boolean ingresarEfectivo(double denominacion) {
+        return efectivo.containsKey(denominacion);
     }
 
-    public void ingresarTarjeta(String tipoTarjeta) {
-        System.out.println("Tarjeta ingresada: " + tipoTarjeta);
+    public boolean ingresarTarjeta(String tipoTarjeta) {
+        return tipoTarjeta != null && (tipoTarjeta.equals("MONEDERO") || tipoTarjeta.equals("BANCARIA"));
     }
 
-    public double getEfectivo() {
-        return efectivo;
+    public Map<Double, Integer> getEfectivo() {
+        return new HashMap<>(efectivo);
     }
 
     public double getTarjetaMonedero() {
@@ -65,7 +63,10 @@ public class Usuario {
 
     public void mostrarSaldos() {
         System.out.println("Saldo actual");
-        System.out.println("Efectivo: €" + efectivo);
+        System.out.println("Efectivo:");
+        for (Map.Entry<Double, Integer> entry : efectivo.entrySet()) {
+            System.out.println("Denominación: €" + entry.getKey() + " - Cantidad: " + entry.getValue());
+        }
         System.out.println("Tarjeta Monedero: €" + tarjetaMonedero);
         System.out.println("Tarjeta Bancaria: €" + tarjetaBancaria);
     }
