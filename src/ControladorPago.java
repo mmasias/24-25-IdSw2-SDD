67a6e6e00f601362b457859bbf4f8ce19f9ad2f0
JLRG806 <jlrg.dev@gmail.com>
2025-04-16 13:14:55 +0200

    feat: Implement ControladorPago class for managing cash and card payments

---
+++ src/ControladorPago.java
@@ -0,0 +1,39 @@
+package src;
+
+public class ControladorPago {
+
+    PagoEfectivo pagoEfectivo;
+    PagoTarjeta pagoTarjeta;
+
+    public ControladorPago() {
+        this.pagoEfectivo = null;
+        this.pagoTarjeta = null;
+    }
+
+    public void iniciarPagoEfectivo(double monto) {
+        this.pagoEfectivo = new PagoEfectivo(monto);
+    }
+
+    public void agregarEfectivo(Efectivo efectivo) {
+            if (pagoEfectivo == null) {
+                System.out.println("Inicie el pago en efectivo primero.");
+                return;
+            }
+            pagoEfectivo.ingresarEfectivo(efectivo);
+            if (pagoEfectivo.validarEfectivo()) {
+                System.out.println("Pago en efectivo validado.");
+            } else {
+                System.out.println("Monto insuficiente.");
+            }
+    }
+
+    public void realizarPagoTarjeta(Tarjeta tarjeta, double monto) {
+        this.pagoTarjeta = new PagoTarjeta(tarjeta, monto);
+        if (pagoTarjeta.realizarPago()) {
+            System.out.println("Pago con tarjeta realizado exitosamente.");
+        } else {
+            System.out.println("Fondos insuficientes en la tarjeta.");
+        }
+    }
+
+}
