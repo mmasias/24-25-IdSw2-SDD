a2be7e1d4ea46ecda0a5cd33c2a1dae0f2b5902c
TheMoys <diegomoys01@gmail.com>
2025-04-16 14:14:26 +0200

    Refactor: Eliminar atributo precio y métodos relacionados de la clase Producto

--- src/Producto.java
+++ src/Producto.java
@@ -2,12 +2,10 @@ package src;
 
 public class Producto {
     private String nombre;
-    private double precio;
     private String fecha_expiracion;
 
-    public Producto(String nombre, double precio, String fecha_expiracion) {
+    public Producto(String nombre, String fecha_expiracion) {
         this.nombre = nombre;
-        this.precio = precio;
         this.fecha_expiracion = null;
     }
 
@@ -16,12 +14,6 @@ public class Producto {
     }
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-    public double getPrecio() {
-        return precio;
-    }
-    public void setPrecio(double precio) {
-        this.precio = precio;
     }
     public String getFecha_expiracion() {
         return fecha_expiracion;
