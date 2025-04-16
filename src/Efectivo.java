bad1d5b0a8b77612355fc3678666d88242b6ff85
JLRG806 <jlrg.dev@gmail.com>
2025-04-16 14:37:37 +0200

    feat: Remove MONEDA enum to simplify currency handling

--- src/Efectivo.java
+++ src/Efectivo.java
@@ -1,21 +1,38 @@
 package src;
 
-
 public class Efectivo {
     private double denominacion;
     private EfectivoAceptado tipoEfectivo;
+    private Tipo tipo;
+
+    public enum Tipo {
+        BILLETE,
+        MONEDA
+    }
 
     public Efectivo(EfectivoAceptado tipoEfectivo) {
         this.denominacion = tipoEfectivo.getValor();
         this.tipoEfectivo = tipoEfectivo;
     }
 
+    public Efectivo(double denominacion, Tipo tipo) {
+        this.denominacion = denominacion;
+        this.tipo = tipo;
+    }
+
+    public void setDenominacion(double denominacion) {
+        this.denominacion = denominacion;
+    }
+
     public double getDenominacion() {
         return denominacion;
     }
 
-    public EfectivoAceptado getTipoEfectivo() {
-        return tipoEfectivo;
+    public Tipo getTipo() {
+        return tipo;
     }
 
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
 }
