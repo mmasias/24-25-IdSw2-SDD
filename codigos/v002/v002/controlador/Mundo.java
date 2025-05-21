7b4d019780ef3b4a8b5e6c0e4103efc1a11083e8
MAURA MARTINEZ NODA <Mauramartineznoda@gmail.com>
2025-05-21 21:11:55 +0200

    refactor: remove manual cat movement and clean up Mundo class

--- codigos/v002/v002/controlador/Mundo.java
+++ codigos/v002/v002/controlador/Mundo.java
@@ -1,8 +1,8 @@
 package v002.controlador;
 
 
-import java.util.Scanner;
 
+import java.util.Scanner;
 import v002.modelo.entidades.Aspiradora;
 import v002.modelo.entidades.Bateria;
 import v002.modelo.entidades.Gato;
@@ -17,21 +17,9 @@ public class Mundo {
         Aspiradora aspiradora = new Aspiradora(bateria, vista);
         Gato gato = new Gato(habitacion, aspiradora, vista);
 
-        habitacion.getZona(5, 5).ensuciar();
-        habitacion.getZona(10, 3).ensuciar();
-        habitacion.getZona(10, 3).ensuciar();
-        habitacion.getZona(15, 7).ensuciar();
-        habitacion.getZona(15, 6).ensuciar();
-        habitacion.getZona(15, 7).ensuciar();
-        habitacion.getZona(24, 8).ensuciar();
-        habitacion.getZona(20, 7).ensuciar();
-        habitacion.getZona(10, 3).ensuciar();
-        habitacion.getZona(15, 2).ensuciar();
-        habitacion.getZona(15, 1).ensuciar();
-        habitacion.getZona(12, 9).ensuciar();
 
         Scanner scanner = new Scanner(System.in);
-        String input;
+    
 
         for (int i = 0; i < 5000; i++) {
             vista.mostrarPaso(i + 1);
@@ -41,30 +29,7 @@ public class Mundo {
                     gato = new Gato(habitacion, aspiradora, vista);
                 }
             } else {
-                System.out.println("Controles del gato:");
-                System.out.println("W - Arriba");
-                System.out.println("S - Abajo");
-                System.out.println("A - Izquierda");
-                System.out.println("D - Derecha");
-                System.out.println("Enter - Saltar turno");
-                
-                input = scanner.nextLine().toUpperCase();
-                if (!input.isEmpty()) {
-                    switch (input.charAt(0)) {
-                        case 'W':
-                            gato.moverManualmente(0);
-                            break;
-                        case 'S':
-                            gato.moverManualmente(1);
-                            break;
-                        case 'A':
-                            gato.moverManualmente(2);
-                            break;
-                        case 'D':
-                            gato.moverManualmente(3);
-                            break;
-                    }
-                }
+              gato.calcularMovimiento(habitacion);
             }
 
             aspiradora.actuar(habitacion);
@@ -76,6 +41,9 @@ public class Mundo {
                     aspiradora.getPosicionX(),
                     aspiradora.getPosicionY(),
                     gato);
+            // Pausa para que el usuario pueda ver el estado del mundo
+            System.out.println("Presiona Enter para continuar...");
+            scanner.nextLine();
         }
 
         scanner.close();
