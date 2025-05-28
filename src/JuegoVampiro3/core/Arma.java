f3f1650d431f746edbe8ccf7b7250c2b6fdb9348
Alejandro Vinas <alejandro.vinas@alumnos.uneatlantico.es>
2025-05-28 17:13:53 +0200

    New version of arma.java

--- src/JuegoVampiro3/core/Arma.java
+++ src/JuegoVampiro3/core/Arma.java
@@ -1,5 +1,9 @@
-package JuegoVampiro2.core;
+package JuegoVampiro3.core;
 
+/**
+ * Clase que representa un arma del guerrero.
+ * Principio SRP: Responsabilidad única - representar un arma específica.
+ */
 public class Arma extends Ataque {
     private String nombre;
 
@@ -8,6 +12,7 @@ public class Arma extends Ataque {
         this.nombre = nombre;
     }
 
+    @Override
     public String getNombre() {
         return nombre;
     }
@@ -15,7 +20,6 @@ public class Arma extends Ataque {
     @Override
     public String toString() {
         // Para mostrar en la lista de armas en VistaConsola
-        return nombre + " (Daño: " + getDaño() + ", Éxito: " + super.probabilidadExito + "%)"; 
-        // Acceder a probabilidadExito directamente o hacerla protected/añadir getter en Ataque
+        return nombre + " (Daño: " + getDaño() + ", Éxito: " + getProbabilidadExito() + "%)"; 
     }
 } 
