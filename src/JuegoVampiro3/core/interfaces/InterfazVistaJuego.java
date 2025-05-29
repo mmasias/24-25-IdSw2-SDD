813eb6af7227cf3b8d34b6551e49163bf5ca5cdd
Gabriel Sicacha Palmar <66096194+Gbrl413@users.noreply.github.com>
2025-05-29 22:07:12 +0200

    Update and rename VistaJuego.java to InterfazVistaJuego.java

--- src/JuegoVampiro3/core/interfaces/InterfazVistaJuego.java
+++ src/JuegoVampiro3/core/interfaces/InterfazVistaJuego.java
@@ -4,7 +4,7 @@ import JuegoVampiro3.core.Arma;
 import JuegoVampiro3.ui.CredencialesUsuario;
 import java.util.List;
 
-public interface VistaJuego {
+public interface InterfazVistaJuego {
     void mostrarMensaje(String mensaje);
     void mostrarTitulo();
     void mostrarMenuInicial();
