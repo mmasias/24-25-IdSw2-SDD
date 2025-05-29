4a4bda75c1cdcfb2f77c5a8f018354581d53a5b4
Gabriel Sicacha Palmar <66096194+Gbrl413@users.noreply.github.com>
2025-05-29 22:06:30 +0200

    Update and rename Luchador.java to InterfazLuchador.java

--- src/JuegoVampiro3/core/interfaces/InterfazLuchador.java
+++ src/JuegoVampiro3/core/interfaces/InterfazLuchador.java
@@ -2,7 +2,7 @@ package JuegoVampiro3.core.interfaces;
 
 import JuegoVampiro3.core.Ataque;
 
-public interface Luchador extends Personaje {
+public interface InterfazLuchador extends InterfazPersonaje {
     Ataque seleccionarAtaque(int indice);
     boolean puedeAtacar();
 } 
